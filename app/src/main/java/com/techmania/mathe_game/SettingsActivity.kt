617abf4d118c94fe3240a65e86f079e802d8721a
GitHub-Leon <70367593+GitHub-Leon@users.noreply.github.com>
2022-05-28 16:23:59 +0200

    added german language support
    added german language support
    added automatic change of language via in app settings
    added automatic loading of language preference settings on startup

--- app/src/main/java/com/techmania/mathe_game/SettingsActivity.kt
+++ app/src/main/java/com/techmania/mathe_game/SettingsActivity.kt
@@ -1,29 +1,65 @@
 package com.techmania.mathe_game
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentContainerView
+import androidx.preference.PreferenceManager
 import com.techmania.mathe_game.fragments.SettingsFragment
+import java.util.*
+
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var fragmentContainerView : FragmentContainerView
+    private lateinit var prefs : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         initViews()
+        registerLanguageChangeListener()
 
         supportActionBar?.title = "Settings" //set title of actionbar to Settings
     }
 
+    private fun registerLanguageChangeListener() {
+        val spChanged =
+            OnSharedPreferenceChangeListener { sharedPreferences, key ->
+                setLocale(sharedPreferences.getString(key, ""))
+                setSettingsContainerView()
+            }
+
+        prefs.registerOnSharedPreferenceChangeListener(spChanged)
+    }
+
     private fun initViews() {
         fragmentContainerView = findViewById(R.id.fragmentContainerView)
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-        //start transaction
+        setSettingsContainerView()
+    }
+
+    private fun setSettingsContainerView() {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragmentContainerView, SettingsFragment())
             .commit()
+    }
+
+    private fun setLocale(languageCode: String?) {
+        val config = resources.configuration
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+            config.setLocale(locale)
+        else
+            config.locale = locale
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            createConfigurationContext(config)
+        resources.updateConfiguration(config, resources.displayMetrics)
     }
 }
--- app/src/main/java/com/techmania/mathe_game/SplashScreenActivity.kt
+++ app/src/main/java/com/techmania/mathe_game/SplashScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.techmania.mathe_game
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
@@ -9,6 +10,8 @@ import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
+import java.util.*
 
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -22,8 +25,28 @@ class SplashScreenActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN) //Hide Status bar
         setContentView(R.layout.activity_splash_screen)
 
+        setAppLanguage()
+
         setupAnimations()
         startMainMenu()
+    }
+
+    private fun setAppLanguage() {
+        setLocale(PreferenceManager.getDefaultSharedPreferences(this).getString("language", ""))
+    }
+
+    private fun setLocale(languageCode: String?) {
+        val config = resources.configuration
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+            config.setLocale(locale)
+        else
+            config.locale = locale
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            createConfigurationContext(config)
+        resources.updateConfiguration(config, resources.displayMetrics)
     }
 
     private fun startMainMenu() {
