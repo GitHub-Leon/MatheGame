524397020237a4f62532543d3652c2528ee7fe9f
Daniela Ragger <danielarag@edu.aau.at>
2022-05-29 09:22:02 +0200

    Font,Spinner,Division
    Textfont, Spinner, Division

---
+++ app/src/main/java/com/techmania/mathe_game/Division.kt
@@ -0,0 +1,11 @@
+package com.techmania.mathe_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Division : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_division)
+    }
+}
