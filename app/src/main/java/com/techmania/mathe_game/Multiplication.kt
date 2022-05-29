2db728ac55d32fd9eca5c5b4afd981c87c46bb07
GitHub-Leon <70367593+GitHub-Leon@users.noreply.github.com>
2022-05-29 11:52:11 +0200

    fixed bugs

--- app/src/main/java/com/techmania/mathe_game/Multiplication.kt
+++ app/src/main/java/com/techmania/mathe_game/Multiplication.kt
@@ -11,7 +11,6 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
